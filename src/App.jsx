26a01d089eeb8f445bfea624ed7cf9f98ba4d983
bon110603 <velmontejustineofficial@gmail.com>
2026-09-19 01:19:54 +0800

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 export default function () {
-  const apiUrl = import.meta.env.VITE_API_URL;
-
   return (
     <div>
-      <h1>Environment Variable</h1>
+      <h1>Environment Variables</h1>
 
-      <p>{apiUrl}</p>
+      <pre>
+        {JSON.stringify(import.meta.env, null, 2)}
+      </pre>
     </div>
   );
 }
