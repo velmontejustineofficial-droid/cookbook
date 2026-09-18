acf44df6b1897dc2527601aa240196a6290b8e1f
bon110603 <velmontejustineofficial@gmail.com>
2026-09-19 01:29:58 +0800

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function () {
   return (
     <div>
       <h1>Environment Variables</h1>
-
+      <h2>finsih</h2>
       <pre>
         {JSON.stringify(import.meta.env, null, 2)}
       </pre>
